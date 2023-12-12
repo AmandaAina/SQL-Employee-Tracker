1cf89d49699c80a6ed98bf60feec36301b6371c0
Amanda Aina <amandaade92@yahoo.com>
2023-12-11 20:34:37 -0500

    add more cases

--- queries.js
+++ queries.js
@@ -10,7 +10,25 @@ class Database {
     return connection.promise().query('INSERT INTO department (name) VALUES (?)', [name]);
   }
 
-  // Add similar functions for roles and employees
+  static getAllRoles() {
+    return connection.promise().query('SELECT * FROM role');
+  }
+
+  static addRole(title, salary, department) {
+    return connection.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, department]);
+  }
+
+  static getAllEmployees() {
+    return connection.promise().query('SELECT * FROM employee');
+  }
+
+  static addEmployee(first_name, last_name, role, manager) {
+    return connection.promise().query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, role, manager]);
+  }
+
+  static updateEmployeeRole(employee_id, new_role) {
+    return connection.promise().query('UPDATE employee SET role_id = ? WHERE id = ?', [new_role, employee_id]);
+  }
 }
 
 module.exports = Database;
